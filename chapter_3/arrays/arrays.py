cb94414e590ea4a466b931c993d8191eaf105189
Alejandro Guirao <lekumberri@gmail.com>
2015-10-30 10:10:51 +0100

    Add numpy example

---
+++ chapter_3/arrays/arrays.py
@@ -0,0 +1,9 @@
+import numpy as np
+print(np.array([1, 4, 2, 5, 3]))
+print(np.zeros(10, dtype=np.int))
+print(np.zeros((3, 5), dtype=np.int))
+print(np.full((3, 5), np.pi))
+print(np.arange(3, 20, 2))
+print(np.linspace(3, 20, 5))
+print(np.random.random((3, 20)))
+print(np.eye(8))
