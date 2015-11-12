91110851464cc07b4085910d5ad9efd340314063
Alejandro Guirao <lekumberri@gmail.com>
2015-11-12 10:21:21 +0100

    Add trigonometric and logarithmic examples

---
+++ chapter_3/ufuncs/logs.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+x = [1, 2, 3]
+print("x =", x)
+print("e^x =", np.exp(x))
+print("2^x =", np.exp2(x))
+print("3^x =", np.power(3, x))
+
+x = [1, 2, 4, 10]
+print("x =", x)
+print("ln(x) =", np.log(x))
+print("log2(x) =", np.log2(x))
+print("log10(x) =", np.log10(x))
---
+++ chapter_3/ufuncs/trigos.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+theta = np.linspace(0, np.pi, 3)
+print("theta =", theta)
+print("sin(theta) = ", np.sin(theta))
+print("cos(theta) = ", np.cos(theta))
+print("tan(theta) = ", np.tan(theta))
+x = [-1, 0, 1]
+print("x = ", x)
+print("arcsin(x) = ", np.arcsin(x))
+print("arccos(x) = ", np.arccos(x))
+print("arctan(x) = ", np.arctan(x))
