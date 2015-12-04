ddcc2b423563d5f7f0be14fa915f432bde174e1e
Alejandro Guirao <lekumberri@gmail.com>
2015-12-04 09:05:22 +0100

    Add additional Series examples

--- chapter_4/pandas_series/pandas_series_example.py
+++ chapter_4/pandas_series/pandas_series_example.py
@@ -3,3 +3,11 @@ import pandas as pd
 
 data = pd.Series([0.25, 0.5, 0.75, 1.0], index=['a', 'b', 'c', 'd'])
 print(data)
+population_dict = {'California': 38332521,
+        'Texas': 26448193,
+        'New York': 19651127,
+        'Florida': 19552860,
+        'Illinois': 12882135}
+population = pd.Series(population_dict)
+print(population)
+print(pd.Series({2:'a', 1:'b', 3:'c'}, index=[3, 2]))
