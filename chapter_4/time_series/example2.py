6b901afdea7d9e57781b2d03a2f01dc8f62783c1
Alejandro Guirao <lekumberri@gmail.com>
2016-02-08 08:27:24 +0100

    Add more examples of Timestamp and DatetimeIndex

--- chapter_4/time_series/example2.py
+++ chapter_4/time_series/example2.py
@@ -5,3 +5,8 @@ index = pd.DatetimeIndex(['2014-07-04', '2014-08-04',
                           '2015-07-04', '2015-08-04'])
 data = pd.Series([0, 1, 2, 3], index=index)
 print(data)
+dates = pd.to_datetime([pd.datetime(2015, 7, 3), '4th of July, 2015',
+    '2015-Jul-6', '07-07-2015', '20150708'])
+print(dates)
+print(dates.to_period('D'))
+print(dates - dates[0])
